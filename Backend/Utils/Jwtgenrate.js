909476ff82bf08453d7f3abd123f05e3c77b82c5
Krish vaswani <147949579+Kri24sh@users.noreply.github.com>
2024-05-03 11:02:58 +0530

    Update Jwtgenrate.js

--- Backend/Utils/Jwtgenrate.js
+++ Backend/Utils/Jwtgenrate.js
@@ -2,13 +2,13 @@ import jwt from 'jsonwebtoken'
 
 const generateTokenAndSetCookie = (userId, res) => {
 	const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-		expiresIn: "15d",
+		expiresIn: "40d",
 	});
 
 	res.cookie("jwt", token, {
 		maxAge: 15 * 24 * 60 * 60 * 1000, // MS
 		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-		sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+		
 		
 	});
 };
