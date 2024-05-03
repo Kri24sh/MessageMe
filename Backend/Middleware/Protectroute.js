b6e50d6f40694ec50e433e6b790e7c155298f52f
Krish vaswani <147949579+Kri24sh@users.noreply.github.com>
2024-05-03 11:16:04 +0530

    Update Protectroute.js

--- Backend/Middleware/Protectroute.js
+++ Backend/Middleware/Protectroute.js
@@ -6,7 +6,7 @@ const protectRoute = async (req, res, next) => {
 	try {
 		const token = req.cookies.jwt;
 
-		console.log(token);
+		
 		
 		if (!token) {
 			return res.status(401).json({ error: "Unauthorized - No Token Provided" });
